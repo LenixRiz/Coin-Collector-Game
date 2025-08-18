db30ff65806134818e13cc93e1fa038fe4382caa
Rizky Fajar Maulana <rizky.fajarbusiness945@gmail.com>
2025-08-18 22:03:46 +0700

    Add visuals

--- Assets/Scripts/DangerZone.cs
+++ Assets/Scripts/DangerZone.cs
@@ -1,11 +1,35 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class DangerZone : MonoBehaviour
 {
     [SerializeField] private float damage = 25f;
+    [SerializeField] private float speed = 5f;
+    private float distance;
+    [SerializeField] private GameObject player;
 
     public float GetDamage()
     {
         return damage;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (player != null)
+        {
+            distance = Vector2.Distance(transform.position, player.transform.position);
+            Vector2 direction = player.transform.position - transform.position;
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            Debug.Log("Player not found!");
+        }
     }
 }
